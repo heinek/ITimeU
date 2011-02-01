cb493d5ed815f1ae4da747181687603cb9debe09
Yngvar Kristiansen <yngvark@gmail.com>
2011-02-01 13:57:23 +0100

    Added a Timer class

---
+++ ITimeU2.Tests/Models/TimerModelTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TinyBDD.Dsl.GivenWhenThen;
+using TinyBDD.Specification.MSTest;
+using ITimeU2.Models;
+
+namespace ITimeU2.Tests.Models
+{
+    /// <summary>
+    /// Summary description for TimerModelTest
+    /// </summary>
+    [TestClass]
+    public class TimerModelTest : ScenarioClass
+    {
+
+        private static TimerModel timerModel = null;
+
+        [TestMethod]
+        public void We_Have_A_Timer_Model()
+        {
+            Scenario("We have a timer model");
+            Given("we want to create a timer model");
+
+            When("we instantiate the timer model class", 
+                () => timerModel = new TimerModel()
+            );
+
+            Then("we have a timer model", () => 
+                Assert.IsNotNull(timerModel)
+            );
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            StartScenario();
+        }
+ 
+       
+    }
+}
