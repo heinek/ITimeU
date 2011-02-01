f7fb5bb55474d1f7f6b344d89dabe37295528e59
Yngvar Kristiansen <yngvark@gmail.com>
2011-02-01 12:32:32 +0100

    Setup of project

---
+++ ITimeU2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITimeU2.MvcApplication" Language="C#" %>
---
+++ sketches/TimerPage/SilverlightPrototype1/SilverlightPrototype1Screens/Screen 1.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightPrototype1Screens
+{
+	public partial class Screen_1 : UserControl
+	{
+		public Screen_1()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
