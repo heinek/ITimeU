8981353eb32c23f314ac688efda49d54c81e0389
tbjorgum <trond-havard.bjorgum@capgemini.com>
2011-02-10 15:09:57 +0100

    new database script + checkpointOrder update

---
+++ ITimeU/Controllers/CheckpointOrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITimeU.Controllers
+{
+    public class CheckpointOrderController : Controller
+    {
+        //
+        // GET: /CheckpointOrder/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
