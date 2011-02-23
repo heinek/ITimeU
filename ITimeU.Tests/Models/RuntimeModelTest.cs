33213e02cd76a126106fa90e420511f0440492bb
Yngvar Kristiansen <yngvark@gmail.com>
2011-02-23 09:56:06 +0100

    Added an empty RuntimeModelTest

---
+++ ITimeU.Tests/Models/RuntimeModelTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TinyBDD.Dsl.GivenWhenThen;
+using TinyBDD.Specification.MSTest;
+using ITimeU.Models;
+using ITimeU.Logging;
+
+/// TODO:
+
+namespace ITimeU.Tests.Models
+{
+
+    [TestClass]
+    public class RuntimetModelTest : ScenarioClass
+    {
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            StartScenario();
+        }
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            
+        }
+
+    }
+}
