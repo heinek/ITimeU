1fd6c4dc8367bee09a5b08a141b0f1069c24edc0
Arnstein <arnstein.johansen@capgemini.com>
2011-02-24 10:40:51 +0100

    Fixed index and updated timerview

--- ITimeU/Controllers/RuntimeController.cs
+++ ITimeU/Controllers/RuntimeController.cs
@@ -1,25 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using ITimeU.Logging;
 using ITimeU.Tests.Models;
 
 namespace ITimeU.Controllers
 {
     public class RuntimeController : Controller
     {
-     
+
         /// <summary>
         /// Saves the runtime to database.
         /// </summary>
         /// <param name="runtime">The runtime in milliseconds. Maximum value is 2 147 483 647
         /// (Int32.MaxValue), which equals 24,86 days.</param>
         /// <returns>The primary key ID of the database row insterted.</returns>
-        public ActionResult Save(string runtime)
+        public ActionResult Save(string runtime, string checkpointId)
         {
-            RuntimeModel runtimeDb = RuntimeModel.Create(Int32.Parse(runtime));
+            RuntimeModel runtimeDb = RuntimeModel.Create(Int32.Parse(runtime), Int32.Parse(checkpointId));
             string reply = runtimeDb.Id.ToString();
 
             // Show ID of created object (and only that).
--- ITimeU/Scripts/TimerHandler.js
+++ ITimeU/Scripts/TimerHandler.js
@@ -12,16 +12,28 @@ var timer;
 var btnStartStop;
 var btnEdit;
 var btnDelete;
+var btnUpHour;
+var btnUpMin;
+var btnUpSek;
+var btnUpMS;
+var btnDownHour;
+var btnDownMin;
+var btnDownSek;
+var btnDownMS;
+var btnIntermediate;
 var initialid;
 var runtimeid;
 var startBtnText = "Start";
 var stopBtnText = "Stop";
 var restartBtnText = "Restart";
-var tbedit;
+var tbEditHour;
+var tbEditMin;
+var tbEditSek;
+var tbEditMSek;
 var ddlCheckpoints
-
-var btnIntermediate;
 var listIntermediate;
+
+
 // Initialises a Stopwatch instance that displays its time nicely formatted.
 TimerHandler.prototype.showTimer = function (lblTimer) {
     timer = new Stopwatch(function (runtime) {
@@ -32,72 +44,85 @@ TimerHandler.prototype.showTimer = function (lblTimer) {
 }
 
 TimerHandler.prototype.setIntermediateAction = function (_btnIntermediate, _listIntermediate, _ddlCheckpoints) {
-/*
- * Start/Stop/Restart button functionality
-*/
+    btnIntermediate = _btnIntermediate;
+    listIntermediate = _listIntermediate;
     ddlCheckpoints = _ddlCheckpoints;
-TimerHandler.prototype.setStartStopActions = function (
-    _btnStartStop, startFunction, stopFunction, restartFunction)
-{
-    btnStartStop = _btnStartStop;
+    Tools.disable(btnIntermediate);
+    btnIntermediate.click(function () {
+        timer.addIntermediate(function (runtime) {
+            url = "/Timer/SaveRuntime/?runtime=" + runtime + "&checkpointid=" + ddlCheckpoints.val();
+            $.get(url, function (data) {
+                listIntermediate.html(data);
+            });
+        });
+    });
+}
 
+TimerHandler.prototype.setStartStopActions = function (
+    _btnStartStop, startFunction, stopFunction, restartFunction) {
+    btnStartStop = _btnStartStop;
     btnStartStop.bind("click", function () {
         timer.startStop();
-
         if (btnStartStop.val() == startBtnText) {
             // Start is clicked
             if (btnIntermediate) {
                 Tools.enable(btnIntermediate);
             }
-
             btnStartStop.val(stopBtnText);
-
             startFunction();
-        } else if (btnStartStop.val() == stopBtnText) {
+        } 
+        else if (btnStartStop.val() == stopBtnText) {
             // Stop is clicked
-
             if (btnIntermediate) {
                 Tools.disable(btnIntermediate);
-            url = "/Timer/SaveRuntime/?runtime=" + runtime + "&checkpointid=" + ddlCheckpoints.val();
+                url = "/Timer/SaveRuntime/?runtime=" + runtime + "&checkpointid=" + ddlCheckpoints.val();
             }
-
             btnStartStop.val(restartBtnText);
-
             stopFunction();
-        } else if (btnStartStop.val() == restartBtnText) {
+        } 
+        else if (btnStartStop.val() == restartBtnText) {
             // Restart is clicked
-
             if (btnIntermediate) {
                 Tools.enable(btnIntermediate);
             }
-        listIntermediate.html("");
+            listIntermediate.html("");
             if (listIntermediate) {
                 Tools.emptyList(listIntermediate);
             }
-
             timer.resetLap();
             btnStartStop.val(stopBtnText);
-
             restartFunction();
         }
         Tools.disable(btnReset);
     });
-
-
 }
 
-TimerHandler.prototype.setEditAction = function (_listIntermediates, _btnEdit, _tbedit) {
+TimerHandler.prototype.setEditAction = function (_listIntermediates, _btnEdit, _tbEditHour, _tbEditMin, _tbEditSek, _tbEditMSek) {
     listIntermediate = _listIntermediates;
     btnEdit = _btnEdit;
-    tbedit = _tbedit;
+    tbEditHour = _tbEditHour;
+    tbEditMin = _tbEditMin;
+    tbEditSek = _tbEditSek;
+    tbEditMSek = _tbEditMSek;
     btnEdit.bind("click", function () {
-        url = "/Timer/EditRuntime/?orginalruntimeid=" + initialid + "&newruntime=" + tbedit.val();
+        url = "/Timer/EditRuntime/?orginalruntimeid=" + initialid + "&hour=" + tbEditHour.val() + "&min=" + tbEditMin.val() + "&sek=" + tbEditSek.val() + "&msek=" + tbEditMSek.val();
         $.get(url, function (data) {
             listIntermediate.html(data);
         });
-        tbedit.val("");
+        tbEditHour.val("");
+        tbEditMin.val("");
+        tbEditSek.val("");
+        tbEditMSek.val("");
         Tools.disable(btnEdit);
         Tools.disable(btnDelete);
+        Tools.disable(btnUpHour);
+        Tools.disable(btnUpMin);
+        Tools.disable(btnUpSek);
+        Tools.disable(btnUpMS);
+        Tools.disable(btnDownHour);
+        Tools.disable(btnDownMin);
+        Tools.disable(btnDownSek);
+        Tools.disable(btnDownMS);
     });
 }
 
@@ -109,28 +134,174 @@ TimerHandler.prototype.setDeleteAction = function (_listIntermediates, _btnDelet
         $.get(url, function (data) {
             listIntermediate.html(data);
         });
-        tbedit.val("");
+        tbEditHour.val("");
         Tools.disable(btnEdit);
         Tools.disable(btnDelete);
+        Tools.disable(btnUpHour);
+        Tools.disable(btnUpMin);
+        Tools.disable(btnUpSek);
+        Tools.disable(btnUpMS);
+        Tools.disable(btnDownHour);
+        Tools.disable(btnDownMin);
+        Tools.disable(btnDownSek);
+        Tools.disable(btnDownMS);
     });
 }
 
-TimerHandler.prototype.setChangeAction = function (_listIntermediates, _tbedit) {
+TimerHandler.prototype.setChangeAction = function (_listIntermediates, _tbEditHour, _tbEditMin, _tbEditSek, _tbEditMSek, _btnDownHour, _btnDownMin, _btnDownSek, _btnDownMS, _btnUpHour, _btnUpMin, _btnUpSek, _btnUpMS) {
     listIntermediate = _listIntermediates;
-    tbedit = _tbedit;
+    tbEditHour = _tbEditHour;
+    tbEditMin = _tbEditMin;
+    tbEditSek = _tbEditSek;
+    tbEditMSek = _tbEditMSek;
+    btnDownHour = _btnDownHour;
+    btnDownMin = _btnDownMin;
+    btnDownSek = _btnDownSek;
+    btnDownMS = _btnDownMS;
+    btnUpHour = _btnUpHour;
+    btnUpMin = _btnUpMin;
+    btnUpSek = _btnUpSek;
+    btnUpMS = _btnUpMS;
     listIntermediate.bind("change", function () {
-        var str = "";
+        var strh = "";
+        var strm = "";
+        var strs = "";
+        var strms = "";
         var strid = "";
         $("#lstIntermediates :selected").each(function () {
-            strid += $(this).val();
-            str += $(this).text();
+            strid = $(this).val();
+            strh = $(this).text().substring(0, 1);
+            strm = $(this).text().substring(2, 4);
+            strs = $(this).text().substring(5, 7);
+            strms = $(this).text().substring(8, 9);
         });
         initialid = strid;
-        tbedit.val(str);
+        tbEditHour.val(strh);
+        tbEditMin.val(strm);
+        tbEditSek.val(strs);
+        tbEditMSek.val(strms);
         Tools.enable(btnEdit);
         Tools.enable(btnDelete);
+        Tools.enable(btnDownHour);
+        Tools.enable(btnDownMin);
+        Tools.enable(btnDownSek);
+        Tools.enable(btnDownMS);
+        Tools.enable(btnUpHour);
+        Tools.enable(btnUpMin);
+        Tools.enable(btnUpSek);
+        Tools.enable(btnUpMS);
     });
+}
 
+TimerHandler.prototype.setIncrementHourAction = function (_btnUpHour, _tbEditHour) {
+    btnUpHour = _btnUpHour;
+    tbEditHour = _tbEditHour;
+    btnUpHour.bind("click", function () {
+        var time = 0;
+        time = parseInt(tbEditHour.val());
+        time = time + 1;
+        time = time % 24;
+        tbEditHour.val(time);
+    });
+}
+
+TimerHandler.prototype.setIncrementMinAction = function (_btnUpMin, _tbEditMin) {
+    btnUpMin = _btnUpMin;
+    tbEditMin = _tbEditMin;
+    btnUpMin.bind("click", function () {
+        var time = parseInt(tbEditMin.val());
+        time = time + 1;
+        time = time % 60;
+        //        if (timermod < 10)
+        //            str = "0" + timermod;
+        //        else
+        //            str = timermod;
+        tbEditMin.val(time);
+    });
+}
+
+TimerHandler.prototype.setIncrementSekAction = function (_btnUpSek, _tbEditSek) {
+    btnUpSek = _btnUpSek;
+    tbEditSek = _tbEditSek;
+    btnUpSek.bind("click", function () {
+        var time = 0;
+        time = parseInt(tbEditSek.val());
+        time = time + 1;
+        time = time % 60;
+        //tbEditSek.val((time < 10 ? "0" : "") + time);
+        tbEditSek.val(time);
+    });
+}
+
+TimerHandler.prototype.setIncrementMSAction = function (_btnUpMS, _tbEditMS) {
+    btnUpMS = _btnUpMS;
+    tbEditMS = _tbEditMS;
+    btnUpMS.bind("click", function () {
+        var time = 0;
+        time = parseInt(tbEditMS.val());
+        time = time + 1;
+        time = time % 10;
+        tbEditMS.val(time);
+    });
+}
+
+TimerHandler.prototype.setDecrementHourAction = function (_btnUpHour, _tbEditHour) {
+    btnUpHour = _btnUpHour;
+    tbEditHour = _tbEditHour;
+    btnUpHour.bind("click", function () {
+        var time = 0;
+        time = parseInt(tbEditHour.val());
+        time = time - 1;
+        if (time == -1) {
+            time = 23;
+        }
+        tbEditHour.val(time);
+    });
+}
+
+TimerHandler.prototype.setDecrementMinAction = function (_btnUpMin, _tbEditMin) {
+    btnUpMin = _btnUpMin;
+    tbEditMin = _tbEditMin;
+    btnUpMin.bind("click", function () {
+        var time = 0;
+        time = parseInt(tbEditMin.val());
+        time = time - 1;
+        if (time == -1) {
+            time = 59;
+        }
+        //tbEditMin.val((time < 10 ? "0" : "") + time);
+        tbEditMin.val(time);
+    });
+}
+
+TimerHandler.prototype.setDecrementSekAction = function (_btnUpSek, _tbEditSek) {
+    btnUpSek = _btnUpSek;
+    tbEditSek = _tbEditSek;
+    btnUpSek.bind("click", function () {
+        var time = 0;
+        time = parseInt(tbEditSek.val());
+        time = time - 1;
+        if (time == -1) {
+            time = 59;
+        }
+        //tbEditSek.val((time < 10 ? "0" : "") + time);
+        tbEditSek.val(time);
+    });
+}
+
+TimerHandler.prototype.setDecrementMSAction = function (_btnUpMS, _tbEditMS) {
+    btnUpMS = _btnUpMS;
+    tbEditMS = _tbEditMS;
+    btnUpMS.bind("click", function () {
+        var time = 0;
+        time = parseInt(tbEditMS.val());
+        time = time - 1;
+        if (time == -1) {
+            time = 9;
+        }
+        tbEditMS.val(time);
+    });
+}
 TimerHandler.prototype.setStartStopActions = function (_btnStartStop, startFunction, stopFunction) {
     btnStartStop = _btnStartStop;
     btnStartStop.bind("click", function () {
@@ -155,8 +326,5 @@ TimerHandler.prototype.setStartStopActions = function (_btnStartStop, startFunct
             }
             stopFunction();
         }
-    });
-
-        });
     });
 }
