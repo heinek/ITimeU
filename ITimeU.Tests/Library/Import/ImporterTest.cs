0e580747c3f519637469bba9ce81c43c39fd05b2
Yngvar Kristiansen <yngvark@gmail.com>
2011-02-25 14:35:25 +0100

    Added tests for FriRes Importer.

---
+++ ITimeU.Tests/Library/Import/ImporterTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TinyBDD.Dsl.GivenWhenThen;
+using TinyBDD.Specification.MSTest;
+using System.Data.OleDb;
+using System.Data;
+
+namespace ITimeU.Tests.Library
+{
+    [TestClass]
+    public class ImporterTest
+    {
+        [TestMethod]
+        public void Test_Import_Participants_From_Access()
+        {
+            var importer = new Importer("ImporterTest.mdb");
+            //importer
+        }
+
+    }
+}
