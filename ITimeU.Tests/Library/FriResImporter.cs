27816cebe3d94a64c0d4a525169a65ba95988f67
Yngvar Kristiansen <yngvark@gmail.com>
2011-02-25 14:35:25 +0100

    Added tests for FriRes Importer.

---
+++ ITimeU.Tests/Library/FriResImporter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ITimeU.Tests.Library
+{
+    class FriResImporter
+    {
+        private string p;
+
+        public FriResImporter(string p)
+        {
+            // TODO: Complete member initialization
+            this.p = p;
+        }
+
+        internal List<ITimeU.Models.ParticipantModel> getParticipants()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
