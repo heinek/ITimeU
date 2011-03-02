c7dd82fe904d480da972997f52dc9e6080471d35
Yngvar Kristiansen <yngvark@gmail.com>
2011-03-02 14:17:47 +0100

    Added importer for FriRes. Renamed ParticipantModel to AthleteModel, as this will be used for model for imported persons.

---
+++ ITimeU/Models/AthleteModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ITimeU.Models
+{
+    [Serializable]
+    public class AthleteModel
+    {
+        public int Id { get; private set; }
+        private bool dbEntryCreated
+        {
+            get
+            {
+                if (Id == 0)
+                    return false;
+                else
+                    return true;
+            }
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string PostalAddress { get; private set; }
+        public string PostalCode { get; private set; }
+        public string PostalPlace { get; private set; }
+        public string Phone { get; private set; }
+        public string EMail { get; private set; }
+        public int Club { get; private set; }
+        public DateTime Birthday { get; private set; }
+        public string Gender { get; private set; }
+
+        public static AthleteModel GetById(int id)
+        {
+            return null;
+        }
+
+        public AthleteModel(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = 0;
+        }
+
+        public void SaveToDb()
+        {
+            
+        }
+
+    }
+}
