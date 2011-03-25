dce660641417d20b4b4e6da8741f42929946275e
unknown <ksoomro@.corp.capgemini.com>
2011-03-25 14:19:04 +0100

    Added Successful Status Message and Error Message in Checkpoint
    Added Successful Status Message in Race

--- ITimeU/Controllers/CheckpointController.cs
+++ ITimeU/Controllers/CheckpointController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ITimeU.Models;
@@ -9,6 +9,8 @@ namespace ITimeU.Controllers
 {
     public class CheckpointController : Controller
     {
+        public const string KEY_A_CHECKPOINT_WAS_CREATED_SUCCESSFULLY = "CheckpointCreatedSuccessfully";
+        public const string KEY_NAME_EMPTY = "CheckpointNameEmpty";
         //
         // GET: /Checkpoint/
 
@@ -22,16 +24,57 @@ namespace ITimeU.Controllers
 
         public ActionResult Create()
         {
+            if (CheckpointWasCreatedSuccessfully())
+                ViewBag.CheckpointCreatedSuccessfully = true;
+            else
+                ViewBag.CheckpointCreatedSuccessfully = false;
+
+            if (IsCheckpointNameEmpty())
+                ViewBag.IsCheckpointEmpty = true;
+            else
+                ViewBag.IsCheckpointEmpty = false;
+
             return View("Create", RaceModel.GetRaces());
         }
 
+        private bool CheckpointWasCreatedSuccessfully()
+        {
+            return TempData.Keys.Contains(KEY_A_CHECKPOINT_WAS_CREATED_SUCCESSFULLY) &&
+                (bool)GetTempData(KEY_A_CHECKPOINT_WAS_CREATED_SUCCESSFULLY) == true;
+        }
+
+        private bool IsCheckpointNameEmpty()
+        {
+            return TempData.Keys.Contains(KEY_NAME_EMPTY) &&
+                (bool)GetTempData(KEY_NAME_EMPTY) == true;
+        }
         [HttpPost]
         public ActionResult Create(int raceId, string txtCheckpointName)
         {
-            CheckpointModel model = new CheckpointModel(txtCheckpointName, raceId); // race with raceId exists in the database already.
-            model.SaveToDb();
+            if (String.IsNullOrEmpty(txtCheckpointName))
+            {
+                SetTempData(KEY_NAME_EMPTY, true);
+            }
+            else
+            {
+                CheckpointModel model = new CheckpointModel(txtCheckpointName, raceId); // race with raceId exists in the database already.
+                model.SaveToDb();
+                SetTempData(KEY_A_CHECKPOINT_WAS_CREATED_SUCCESSFULLY, true);
+            }
             return RedirectToAction("Create"); // Redirect in order to reset form values.
         }
+
+
+        private void SetTempData(string key, object value)
+        {
+            TempData[key] = value;
+        }
+
+        private object GetTempData(string key)
+        {
+            return TempData[key]; ;
+        }
+
 
     }
 }
